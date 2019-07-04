59600533d09af79d35247c66efcd06138fdbcc9a
chiranz <chiranpoudyal@gmail.com>
2019-07-04 09:43:35 +0530

    Setting up Database & Server

---
+++ models/Post.js
@@ -0,0 +1,22 @@
+const { model, Schema } = require("mongoose");
+
+const postSchema = new Schema({
+  body: String,
+  username: String,
+  created_at: String,
+  comments: [
+    {
+      body: String,
+      username: String
+    }
+  ],
+  likes: [
+    {
+      username: String,
+      created_at: String
+    }
+  ],
+  user: { type: Schema.Types.ObjectId, ref: "users" }
+});
+
+module.exports = model("Post", postSchema);
