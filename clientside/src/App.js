a624d353a94660075ceb7e5d2f5ce5721b99cd80
chiranz <chiranpoudyal@gmail.com>
2019-07-06 08:54:54 +0530

    Setting up Semantic ui, Pages, Routes

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import MenuBar from "./components/MenuBar";
 
 function App() {
   return (
     <div>
-      <h1>My Microblog</h1>
+      <Router>
+        <MenuBar />
+        <Container>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Container>
+      </Router>
     </div>
   );
 }
