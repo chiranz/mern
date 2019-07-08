c92169a74a5eaa68a01e8b61eb9f46fb64df606f
chiranz <chiranpoudyal@gmail.com>
2019-07-08 08:27:33 +0530

    Transition for added and deleted posts

--- clientside/src/pages/Home.js
+++ clientside/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { useQuery } from "@apollo/react-hooks";
-import { Grid } from "semantic-ui-react";
+import { Grid, Transition } from "semantic-ui-react";
 import { AuthContext } from "../context/auth";
 import PostCard from "../components/PostCard";
 import PostForm from "../components/PostForm";
@@ -33,19 +33,21 @@ function Home() {
         {loading ? (
           <h1>Loading posts....</h1>
         ) : (
-          posts &&
-          posts.map((post, index) => (
-            <Grid.Column
-              key={index}
-              style={{ marginBottom: 20 }}
-              mobile={16}
-              tablet={8}
-              computer={5}
-              stretched
-            >
-              <PostCard post={post} />
-            </Grid.Column>
-          ))
+          <Transition.Group>
+            {posts &&
+              posts.map((post, index) => (
+                <Grid.Column
+                  key={index}
+                  style={{ marginBottom: 20 }}
+                  mobile={16}
+                  tablet={8}
+                  computer={5}
+                  stretched
+                >
+                  <PostCard post={post} />
+                </Grid.Column>
+              ))}
+          </Transition.Group>
         )}
       </Grid.Row>
     </Grid>
